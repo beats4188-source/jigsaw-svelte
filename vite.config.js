d12d5a78d4aeb9f5596fc764b186a99c65e3ac41
Rakshit Narang <rakshitnarang018@gmail.com>
2025-08-19 17:08:15 +0530

    Merge remote

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	base: '/jigsaw-svelte/',
-	plugins: [svelte()]
+  base: './',   // 👈 relative path for assets
+  plugins: [svelte()]
 });
